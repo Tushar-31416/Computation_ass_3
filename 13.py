80c4617ee35d7b995942960ca9f55725ec351233
Tushar-31416 <163893275+Tushar-31416@users.noreply.github.com>
2024-05-12 22:16:30 +0530

    Add files via upload

---
+++ 13.py
@@ -0,0 +1,77 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+# READING THE FILE ******************************************************
+
+with open('noise.txt') as file:
+    f = file.readlines()
+    f = [float(item.rstrip()) for item in f]
+
+
+N = len(f)
+x = [i for i in range(N)]
+#*************************************************************************
+
+# FINDING DFT ***********************************************************
+
+
+dft = np.fft.fft(f,norm='ortho')
+k = np.fft.fftfreq(N)               #JUST GIVES q/N
+
+sorted = []
+for i in range(N):
+    sorted.append([k[i],dft[i]])
+sorted.sort(key=lambda x:x[0])
+
+kf,dftf = [],[]
+for i in range(N):
+    kf.append(sorted[i][0])
+    dftf.append(sorted[i][1])
+
+
+# OBTAINING THE POWER SPECTRUM ********************************************
+
+period = [np.abs(x)**2 for x in dftf]
+
+# BINNING THE SPECTRUM ****************************************************
+
+kmin,kmax = kf[0],kf[-1]
+bin = 50
+delta = (kmax-kmin)/bin
+k_bin,per_bin = [],[]
+
+d = delta
+n = 0
+for i in range(1,bin+1):
+    dftb = 0
+    print('i=',i)
+    while(kf[n]<kf[0]+i*d):
+        dftb = dftb + period[n]
+        n = n+1
+        print('n =',n)
+        if(n==len(kf)):
+            break
+    per_bin.append(dftb)
+    k_bin.append(kf[0]+i*d)
+
+
+
+
+
+
+# PLOTTING EVERYTHING *************************************************************
+
+figure, ax = plt.subplots(2, 2) 
+
+ax[0,0].plot(x,f)
+ax[0,0].set_xlabel('Time')
+ax[0,0].set_ylabel('Data')
+
+ax[0,1].plot(kf,dftf)
+
+ax[1,0].plot(kf,period)
+
+ax[1,1].plot(k_bin,per_bin)
+
+plt.show()
---
+++ 6.py
@@ -0,0 +1,42 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+
+def f(x):
+    return 1
+
+
+xmin = -50
+xmax = 50
+N = 200
+delx = (xmax-xmin)/(N-1)
+
+
+sample =[]
+for i in range(N):
+    sample.append(f(xmin+i*delx))
+
+
+
+
+dft = np.fft.fft(sample,norm='ortho')
+k = (2*np.pi/delx)*np.fft.fftfreq(N)               #JUST GIVES q/N
+
+sorted = []
+
+for i in range(N):
+    sorted.append([k[i],dft[i]])
+
+sorted.sort(key=lambda x:x[0])
+
+final_k,final_dft = [],[]
+for l in range(N):
+    final_dft.append(delx*(N/(2*np.pi))**0.5*np.exp(-1j*xmin*sorted[l][0])*sorted[l][1])
+    final_k.append(sorted[l][0])
+
+
+
+plt.plot(final_k,final_dft,'go',markersize=2)
+plt.xlim(-5,5)
+plt.show()
