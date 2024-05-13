6bbbb750890b67413927718c1583f5fcdd3fd1dc
Tushar-31416 <163893275+Tushar-31416@users.noreply.github.com>
2024-05-13 21:39:12 +0530

    Update q4.c

--- q4.c
+++ q4.c
@@ -4,37 +4,34 @@
 #include <fftw3.h>
 
 #define N 1001
-#define xlim 100
+#define xmin -100
+#define xmax 100
 
 double gaussian(double x) {
     return exp(-x*x);
 }
 
 int main() {
-    fftw_complex in[N], out[N];
+    fftw_complex f[N], dft[N];
     fftw_plan p;
-    double delta = (double) 2 * xlim / (N - 1);
+    double delta = (double) (xmax-xmin) / (N - 1);
 
-    p = fftw_plan_dft_1d(N, in, out, FFTW_FORWARD, FFTW_ESTIMATE);
+    p = fftw_plan_dft_1d(N, f, dft, FFTW_FORWARD, FFTW_ESTIMATE);
 
     for (int i=0; i < N; i++) {
-        in[i] = gaussian(-xlim + i * delta) + I * 0.0;
+        f[i] = gaussian(xmin + i * delta) + I * 0.0;
     }
 
     fftw_execute(p);
-
-    FILE *file = fopen("q4_data.csv","w");
+    FILE *file = fopen("q4_1_data.csv","w");
 
     for (int i = 0; i < N; i++) {
-        fprintf(file, "%g, %g\n", creal(out[i]), cimag(out[i]));
+        fprintf(file, "%g, %g\n", creal(dft[i]), cimag(dft[i]));
     }
 
     fclose(file);
 
     fftw_destroy_plan(p);
     fftw_cleanup();
-
-    printf("Fourier transform data has been written to 'q4_data.csv'.\n");
-
     return 0;
 }
